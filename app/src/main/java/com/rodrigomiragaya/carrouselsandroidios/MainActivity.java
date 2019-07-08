7d8705130af1cef84142d3314d63244eece9febc
Rodrigo <rodrigo.e.miragaya@gmail.com>
2019-07-08 13:56:57 -0300

    Pulido

--- app/src/main/java/com/rodrigomiragaya/carrouselsandroidios/MainActivity.java
+++ app/src/main/java/com/rodrigomiragaya/carrouselsandroidios/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.Request;
